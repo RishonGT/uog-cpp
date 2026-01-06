409edf55fdd913f861804f5558de360a5a93eec1
Cafesua <khiem.nguyen@glasgow.ac.uk>
2026-01-06 05:09:19 +0000

    Create chapter-01 and files inside it

---
+++ chap-01/cpp/define_variables.cpp
@@ -0,0 +1,15 @@
+int main()
+{
+    int x;              // definition of variable x
+    int y;              // definition of variable y
+    // Define multiple variables in one line
+    int a, b;           // note the comma between a and b
+    // This is wrong:
+    // int a, int b;    // wrong (compiler error)
+
+    int a1; double a2; // correct (but not recommended)
+    // correct and recommended -- write in separte lines(easier to read)
+    int b1;
+    double b2;
+    return 0;
+}
---
+++ chap-01/cpp/hello_error.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+   std::cout << "Hello world!"  // we forget the semicolon ";" here.
+   return 0;
+}
