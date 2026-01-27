423ff154c789c7ebab1ec51d2c4c5cdcd071022b
Rishon <rishontom@gmail.com>
2026-01-27 16:57:56 +0000

    gfouq

--- assignments/assignment-01/task_2/linear_function.cpp
+++ assignments/assignment-01/task_2/linear_function.cpp
@@ -1,6 +1,7 @@
 //Function to calculate an linear function with an intercept and gradient.
+#include "linear_function"
 
-double linear_function( double x, double intercept, double gradient)
+double linear_function(double x, double intercept, double gradient)
 {
     double y;
 
--- assignments/assignment-01/task_2/task2.cpp
+++ assignments/assignment-01/task_2/task2.cpp
@@ -1,17 +1,19 @@
-
-
-
+#include <iostream>
+#include "linear_function"
 
 
 int main(){
-    int k = 3;
-    int m = 2;
+    double k = 3.0;
+    double m = 2.0;
 
-    int x[1000];
+    double x[1000];
+    double y[1000];
+    double data[1000][1000];
     for(int i = 1; i <= 1000; i++){
         x[i] = i;
-        
-    } 
+        y[i] = linear_function(x[i], m, k);
 
+        std::cout << y[i] << '\n';
+    } 
     return 0;
 }
