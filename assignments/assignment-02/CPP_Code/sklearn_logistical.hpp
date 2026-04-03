603f81a826684e503414e6397151cc1699ef6bbb
Miller <2721228l@student.gla.ac.uk>
2026-04-03 02:14:24 +0100

    Testing

---
+++ assignments/assignment-02/CPP_Code/sklearn_logistical.hpp
@@ -0,0 +1,17 @@
+#ifndef SKLEARN_LOGISTICAL_HPP
+#define SKLEARN_LOGISTICAL_HPP
+
+
+namespace sklearn_cpp{
+namespace linear_model
+{
+    class LogisticalRegression{
+        private:
+
+        public:
+
+    }
+}
+}
+
+#endif
