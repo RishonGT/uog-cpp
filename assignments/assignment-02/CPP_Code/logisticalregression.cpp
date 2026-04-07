0f4a9ff991d0688467e7e0306cebed596dddd8f3
Miller <2721228l@student.gla.ac.uk>
2026-04-07 02:58:41 +0100

    Initial work with CSV.

--- assignments/assignment-02/CPP_Code/logisticalregression.cpp
+++ assignments/assignment-02/CPP_Code/logisticalregression.cpp
@@ -16,12 +16,22 @@ int main()
     }
 
     std::string strInput{};
+    std::vector<std::vector<double> > data{};
+    
     while (std::getline(inf, strInput))
     {
         std::stringstream lineStream(strInput);
         std::string cell {};
-        std::getline(lineStream, cell, ',');
-        std::cout << cell << "\n";
+        
+        std::vector<double> stringrow {};
+        while(std::getline(lineStream, cell, ','))
+            stringrow.push_back(std::stod(cell));
+        
+        data.push_back(stringrow);
     }    
+
+
+
+    std::cout << data[0].back();
     return 0;
 }
