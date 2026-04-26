116a54180ab2c15a32257611a6fd66add0ec60bf
Miller <2721228l@student.gla.ac.uk>
2026-04-26 15:05:31 +0100

    Rewrote headers

--- assignments/assignment-02/CPP_Code/LogisticalRegression/readCSV.hpp
+++ assignments/assignment-02/CPP_Code/LogisticalRegression/readCSV.hpp
@@ -1,6 +1,8 @@
 #ifndef READCSV_H
 #define READCSV_H
 
+#include <iostream>
+#include <fstream>
 #include <sstream>
 #include <vector>
 
@@ -19,11 +21,65 @@ private:
     int m_header{};
 
 public:
-    ReadCSV(std::string m_filename, char m_delimiter,int m_header);
+ReadCSV(std::string filename, char delimiter,int header)
+    :m_filename{ filename }
+    ,m_delimiter{ delimiter}
+    ,m_header{ header }
+{
+}
 
-    void printfilename() const;
+void printfilename() const
+{
+    std::cout << "Filename:" << m_filename << "\n";
+}
 
-    DataFrame readCSV() const;
+DataFrame readCSV() const
+{
+    std::ifstream inputfile{m_filename};
+    //Check if there is a file
+    if (!inputfile)
+    {
+        std::cerr << "File not found \n";
+        return {};
+    }
+
+    //Skip line(s) for header
+    for (int i{0}; i < m_header; ++i)
+    {
+        std::cout << "Skipped line for header: " << i+1 << "\n";
+        std::string dummyline;
+        std::getline(inputfile, dummyline);
+    }
+    
+
+    //Put data into the struct defined in the header file. 
+    std::string strInput{};
+    DataFrame data{};
+
+    //Acess the row within the line
+    while(std::getline(inputfile, strInput))
+    {
+        std::stringstream lineStream(strInput);
+        std::string cell {};
+
+        //Access the cell within the line
+        std::vector<double> stringrow {};
+        while(std::getline(lineStream, cell, m_delimiter))
+        {
+            //each cell is put into a row vector
+            stringrow.push_back(std::stod(cell));
+        }
+
+        //We are assuming the label is in the last coloumn
+        data.Y.push_back(std::move(stringrow.back()));
+        stringrow.pop_back();
+        data.X.push_back(std::move(stringrow));
+    }
+
+    inputfile.close();
+
+    return data;
+}
 };
 
 #endif
--- assignments/assignment-02/CPP_Code/LogisticalRegression/sklearn_logistical.hpp
+++ assignments/assignment-02/CPP_Code/LogisticalRegression/sklearn_logistical.hpp
@@ -2,6 +2,8 @@
 #define SKLEARN_LOGISTICAL_HPP
 
 #include <vector>
+#include <cmath>
+#include <numeric>
 
 namespace linear_model
 {
@@ -15,24 +17,69 @@ private:
     double m_lambda{0.01};
     
     //dot product of x.w
-    double dot(const std::vector<double> &X) const;
+    double dot(const std::vector<double> &x) const
+    {
+        double result {0.0};
+        for(auto i{0}; i<x.size(); ++i)
+        {
+            result += x[i] * m_weights[i];
+        }
+        return result;
+    }
+
     //sigmoid function
-    double sigmoid(const double &z) const;
+    double sigmoid(const double &z) const
+    {
+        double y_hat {1/(1+(exp(-z)))};
+        return y_hat;
+    }
+
     //prediction or y_hat
-    double predict(const std::vector<double> &x) const;
+    double predict(const std::vector<double> &x) const
+    {
+        double z {dot(x) + m_bias};
+        return sigmoid(z);
+    }
+
     //cross entropy loss
     double xentropyLoss(
-        const std::vector<double> &X,
-        const std::vector<double> &Y
-    ) const;
+        const std::vector<double> &x,
+        const std::vector<double> &y
+    ) const
+    {
+        double loss {0.0};
+        for(auto i{0}; i < x.size(); ++i)
+        {
+            double y_hat = predict(x);
+            loss += y[i] * log(y_hat) + (1- y[i]) * log(1 - y_hat);
+        }
+        loss = -loss / y.size();
+        
+        //Regularization
+        //Sum of Weights
+        double weights{0.0};
+        for(double w: m_weights)
+        {
+            weights += w * w;
+        }
+        loss += m_lambda * weights;
 
-    //TODO: Gradient descent
+        //Bias term
+        loss += m_lambda * m_bias * m_bias;
+        
+        return loss;
+    }
+
     //TODO: Training the data
+    
 
 public:
     //Constructor
-    LogisticalRegression(int features, double learningrate);
-
+    LogisticalRegression(int features, double learningrate)
+    :m_learningrate{learningrate}
+{
+    m_weights.resize(features,0.0);
+}
 
 };
 }
