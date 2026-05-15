bf2225ed6b3dee0c2650eb826396d290ae79488f
Miller <2721228l@student.gla.ac.uk>
2026-05-15 04:51:44 +0100

    Training complete, data split complete

--- assignments/assignment-02/CPP_Code/LogisticalRegression/logisticalregression.cpp
+++ assignments/assignment-02/CPP_Code/LogisticalRegression/logisticalregression.cpp
@@ -4,15 +4,18 @@
 #include <iostream>
 #include <sstream>
 #include <vector>
-
-
+#include <numeric>
+#include <random>
+#include <algorithm>
 
 int main()
 {
     DataFrame ecg_data;
     Read_CSV::read_csv("../../data/ecg.csv",ecg_data.feature,ecg_data.label);
-    std::cout << "Datasets: "<< ecg_data.row() <<" "<< "Features: "<< ecg_data.col() << '\n';
-    
+    std::cout << "Data from CSV: \n";
+    ecg_data.print();
+
+    //Print out first few datasets and features
     for(size_t i{0}; i <= 2; i++)
     {
         for(size_t j{0}; j <= 4; j++)
@@ -22,18 +25,50 @@ int main()
         std::cout << "...\n";
     }
 
-    /*Testing*/
-    DataFrame test_data;
-    LogisticalRegression test_class;
-    test_data.feature = {{1.0,2.0,3.0},{1.0,2.0,3.0},{1.0,2.0,3.0}};
-    test_data.weight = {0.2,0.2,0.2};
-    test_data.label = {1, 1, 1};
-    test_data.bias = 0.5;
+    //Calculate index to split data
+    const size_t train_size = static_cast<size_t>(0.8 * ecg_data.row());
+    //Shuffle Data
+    std::vector<size_t> indices(ecg_data.label.size());
+    std::iota(indices.begin(), indices.end(), 0);
+    std::shuffle(indices.begin(), indices.end(), std::mt19937{42});
+    //Create DataFrames to hold the train and test data
 
-    size_t row_index {1};
-    double z {test_class.predict(test_data.feature,test_data.weight,test_data.bias,row_index)};
-    std::cout << "Z: " << z << '\n';
-    test_class.printState(test_data.feature,test_data.label,test_data.weight,test_data.bias);
+    DataFrame ecg_test;
+    DataFrame ecg_train;
+
+    ecg_train.feature.resize(ecg_data.col());
+    ecg_train.resize(train_size);
+    ecg_test.feature.resize(ecg_data.col());
+    ecg_test.resize(ecg_data.row()-train_size);
     
+    for (size_t j{0}; j < ecg_data.col(); ++j)
+    {
+        for(size_t i=0; i<train_size; ++i)
+        {
+            ecg_train.feature[j][i] = ecg_data.feature[j][indices[i]];
+        }
+        for(size_t i{train_size}; i<ecg_data.row();++i)
+        {
+            ecg_test.feature[j][i-train_size] = ecg_data.feature[j][indices[i]];
+        }
+    }
+
+    for (size_t i{0}; i<train_size;++i)
+    {
+        ecg_train.label[i] = ecg_data.label[indices[i]];
+    }
+    for (size_t i{train_size};i<ecg_data.row();++i)
+    {
+        ecg_test.label[i-train_size] = ecg_data.label[indices[i]];
+    }
+    std::cout << "Split Data: \n";
+    ecg_train.print();
+    ecg_test.print();
+
+
+    LogisticalRegression log_reg;
+    log_reg.train(ecg_train.feature,ecg_train.label);
+
+   
     return 0;
 }
